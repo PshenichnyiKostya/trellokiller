458c7cdcc32fcaa0050eb7ede552a0ee017074ac
PshenichnyiKostya <kpshenichnyi@bk.ru>
2020-10-01 17:15:29 +0300

    Updated client

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {useRoutes} from "./routes"
+import {BrowserRouter} from "react-router-dom"
+import {useAuth} from "./hooks/auth.hook"
+import {AuthContext} from "./context/AuthContext"
+import CopyrightComponent from "./components/CopyrightComponent";
+import NavBar from "./components/NavBar";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+
+}));
+
+function App() {
+    const {login, logout, token, userId} = useAuth()
+    const isAuthenticated =!!token
+    const routes = useRoutes(isAuthenticated)
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+        <AuthContext.Provider value={{
+            login, logout, token, userId, isAuthenticated
+        }}>
+            <BrowserRouter>
+                {<NavBar/>}
+                <div>
+                    {routes}
+                </div>
+            </BrowserRouter>
+            <CopyrightComponent/>
+        </AuthContext.Provider>
+        </div>
+    )
+}
+
+export default App
